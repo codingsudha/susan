b84dab46b86c0228be679d56174954468d95003a
Sudha Bhattarai <coodingsudha@gmail.com>
2024-09-05 15:42:11 -0500

    Initial commit of Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+  // Redirect unknown routes to home or a 404 page
+  { path: '**', redirectTo: '' }
+];
