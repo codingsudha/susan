9c5f3bbf6930332e65299d81c25c348da7751bdb
Sudha Bhattarai <coodingsudha@gmail.com>
2024-09-09 08:58:01 -0500

    Added Research Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
+import { ResearchComponent } from './research/research.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'research', component: ResearchComponent},
   // Redirect unknown routes to home or a 404 page
   { path: '**', redirectTo: '' }
 ];
